6a28b8cceba33677dc4ae4c0e653972e00b7f0c5
charmoniumQ <sag150430@utdallas.edu>
2016-07-16 12:28:40 -0500

    generated admins, supervisors, and contestants (devise defaults)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,23 @@
 Rails.application.routes.draw do
-  get 'admin/index'
+  devise_for :contestants
+  devise_for :supervisors, controllers: {
+    confirmations: 'supervisors/confirmations',
+    omniauths: 'supervisors/omniauth',
+    passwords: 'supervisors/passwords',
+    registrations: 'supervisors/registrations',
+    sessions: 'supervisors/sessions',
+    unlocks: 'supervisors/unlocks'
+  }
 
-  devise_for :admins
+  devise_for :admins, controllers: {
+    confirmations: 'admins/confirmations',
+    omniauths: 'admins/omniauth',
+    passwords: 'admins/passwords',
+    registrations: 'admins/registrations',
+    sessions: 'admins/sessions',
+    unlocks: 'admins/unlocks'
+  }
+
   root to: 'home#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
