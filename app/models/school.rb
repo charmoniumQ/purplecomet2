fc2b31063bb68a1a2a0ed78f704cfd249179be72
charmoniumQ <sag150430@utdallas.edu>
2016-08-19 06:34:09 -0500

    added schools?

--- app/models/school.rb
+++ app/models/school.rb
@@ -3,4 +3,26 @@ class School < ApplicationRecord
 
   validates :name, presence: true
   validates_associated :address
+
+  def self.search(terms)
+    terms.downcase!
+
+    # search for substring
+    School.all.each do |school|
+      if school[:name].downcase.index(terms)
+        return school
+      end
+    end
+
+    # search for all terms, out of order
+    terms_arr = terms.split(' ')
+    School.all.each do |school|
+      if terms_arr.all? { |term| !school[:name].downcase.index(term).nil? }
+        return school
+      end
+    end
+
+    # not found
+    return nil
+  end
 end
--- test/models/school_test.rb
+++ test/models/school_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class SchoolTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "searching schools" do
+    test_data = [
+      {name: "My school", street: "1234 Test", country: "Uganda", search: []},
+
+      # test case-insensitive
+      {name: "Other school", street: "5678 Test", country: "Spain", search: ["other"]},
+
+      # test second-word search and out-of-order word search
+      {name: "Really wicked cool school", street: "1938 Test", country: "England", search: ["wicked", "cool wicked really"]}
+    ]
+
+
+    test_data.each do |test_datum|
+      a = Address.create street: test_datum[:street], country: test_datum[:country]
+      test_datum[:id] = School.create name: test_datum[:name], address: a
+    end
+
+    test_data.each do |test_datum|
+      test_datum[:search].each do |search_term|
+        result = School.search search_term
+        assert_not_nil result, "searching for #{search_term} turned up no results in #{School.all.map { |school| school[:name]}}"
+        assert_equal result[:name], test_datum[:name]
+      end
+    end
+
+  end
 end
